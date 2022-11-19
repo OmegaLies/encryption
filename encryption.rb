8e44d1032bdf46afdf104d2e07117c0c9296d3d0
OmegaLies <omegaunderwall@gmail.com>
2022-11-19 16:00:05 +0300

    SHA2 method

--- encryption.rb
+++ encryption.rb
@@ -9,16 +9,18 @@ puts
 puts "Каким способом зашифровать:"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 
 user_choice = STDIN.gets.to_i
 result =
-  if user_choice == 1
-    Digest::MD5.hexdigest(user_word)
-  elsif user_choice == 2
-    Digest::SHA1.hexdigest(user_word)
+  case user_choice 
+  when 1 then Digest::MD5.hexdigest(user_word)
+  when 2 then Digest::SHA1.hexdigest(user_word)
+  when 3 then Digest::SHA2.hexdigest(user_word)
   else
     "Такого способа нет"
   end
 
 puts "Вот что получилось:"
 puts "#{result}"
+
